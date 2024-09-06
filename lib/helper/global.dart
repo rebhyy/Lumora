9fc662f2db26dc9de76dbb6c2cbe521cc7765efd
rebhyy <a.rebhy10@gmail.com>
2024-09-06 02:48:26 +0100

    Setting up env

--- lib/helper/global.dart
+++ lib/helper/global.dart
@@ -11,5 +11,4 @@ late Size mq;
 //  You need to Update it in your Appwrite Project or comment appwrite code and hardcord key here.
 
 // String apiKey = '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';
-String apiKey =
-    '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';
+String apiKey = '';
